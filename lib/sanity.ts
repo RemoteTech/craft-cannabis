465704c20f65ebc96943e4220448c38869240700
Juan Velez <jvelez@remotetech.nyc>
2025-04-30 01:44:42 -0400

    update

---
+++ lib/sanity.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@sanity/client'
+
+export const sanityClient = createClient({
+  projectId: 'uvuj8y7z',       // Replace with your real project ID
+  dataset: 'production',              // Default dataset name (or yours)
+  apiVersion: '2023-01-01',           // Any recent date
+  useCdn: true,                       // `true` for fast cached reads
+})
